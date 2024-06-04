9961c4ca76bd8436dc6646b72fc397cb2031347c
Ingrid-Garcia <A01754475@tec.mx>
2024-06-04 13:04:53 -0600

    ajuste

--- src/models/ClientsModel.ts
+++ src/models/ClientsModel.ts
@@ -1,52 +1,52 @@
-// import { Model, Sequelize } from 'sequelize';
+import { Model, Sequelize } from 'sequelize';
 
-// interface ClientsAttributes {
-//     Id: number;
-//     nombre: string;
-//     Telefono: string;
-//     email: string;
-// }
+interface ClientsAttributes {
+    Id: number;
+    nombre: string;
+    Telefono: string;
+    email: string;
+}
 
-// module.exports = (sequelize: Sequelize, DataTypes: any) => {
-//     class Cliente extends Model<ClientsAttributes> implements ClientsAttributes {
-//         public Id!: number;
-//         public nombre!: string;
-//         public Telefono!: string;
-//         public email!: string;
+module.exports = (sequelize: Sequelize, DataTypes: any) => {
+    class Cliente extends Model<ClientsAttributes> implements ClientsAttributes {
+        public Id!: number;
+        public nombre!: string;
+        public Telefono!: string;
+        public email!: string;
     
-//         static associate(models: any) {
-//             this.hasMany(models.Poliza, { foreignKey: 'Telefono', as: 'poliza' });
-//         }
-//     }
+        static associate(models: any) {
+            this.hasMany(models.Poliza, { foreignKey: 'Telefono', as: 'poliza' });
+        }
+    }
 
-//     Cliente.init({
-//         Id: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         },
-//         nombre: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         },
-//         Telefono: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//             primaryKey: true
-//         },
-//         email: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         }
-//     }, {
-//         sequelize,
-//         modelName: 'Cliente',
-//         indexes: [
-//             {
-//                 unique: true,
-//                 fields: ['Telefono']
-//             }
-//         ]
-//     },);
+    Cliente.init({
+        Id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Telefono: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        sequelize,
+        modelName: 'Cliente',
+        indexes: [
+            {
+                unique: true,
+                fields: ['Telefono']
+            }
+        ]
+    });
 
-//     return Cliente;
-// }
+    return Cliente;
+}
