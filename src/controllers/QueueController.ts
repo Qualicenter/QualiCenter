9673efce4ab0af513530dcaf55638e3eb459314a
Gustavo Tellez <gusgoosegus1@gmail.com>
2024-06-07 10:36:42 -0600

    Added dynamo update methods for queues
    Added methods to update disconnected calls of a queue to be use in the admin application

--- src/controllers/QueueController.ts
+++ src/controllers/QueueController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import AbstractController from "./AbstractController";
 import QueueData from "../modelsNoSQL/QueueData"; // Import your QueueData model
+import connect from "../services/connectService"; // Import Amazon Connect SDK
 
 // Controller for the QueueData model, which represents the data of all calls in Queue
 class QueueDataController extends AbstractController {
@@ -15,9 +16,11 @@ class QueueDataController extends AbstractController {
 
     protected initRoutes(): void {
         this.router.put("/updateQueueData", this.updateQueueData.bind(this));
+        this.router.get("/getActiveCallsInQueue", this.getActiveCallsInQueue.bind(this));
+        this.router.put("/updateDisconnectedCalls", this.updateDisconnectedCalls.bind(this));
     }
 
-    // Update the inQueue field of a QueueData item
+    // Update the inQueue field of a call in the QueueData table (Used when a call is answered by an agent)
     private async updateQueueData(req: Request, res: Response) {
         const { ContactID } = req.body;
 
@@ -29,6 +32,60 @@ class QueueDataController extends AbstractController {
             res.status(500).send("Internal server error");
         }
     }
+
+    // Get all active calls in the queue
+    private async getActiveCallsInQueue(req: Request, res: Response) {
+        try {
+            const data = await QueueData.scan().where('InQueue').eq(true).exec().promise();
+            res.status(200).json(data);
+        } catch (err) {
+            console.error("Error fetching active queue data:", JSON.stringify(err));
+            res.status(500).send("Internal server error");
+
+        }
+    }
+
+    // Update the inQueue field of a call that has been disconnected before being answered by an agent
+    private async updateDisconnectedCalls(req: Request, res: Response) {
+        try {
+            // Perform scan operation to get active calls in queue
+            const pages = await QueueData.scan().where('InQueue').eq(true).exec().promise();
+            
+            // Iterate over each page to extract ContactIDs
+            const contactIDs: string[] = [];
+            for (const page of pages) {
+                if (page.Items) {
+                    for (const item of page.Items) {
+                        const contactID = item.attrs.ContactID; // Access ContactID from attrs property
+                        contactIDs.push(contactID);
+                    }
+                }
+            }
+
+            // Iterate over each ContactID to check if it has been disconnected
+            for (const contactID of contactIDs) {
+                const params = {
+                    InstanceId: 'e730139b-8673-445e-8307-c3a9250199a2', // Replace with your Amazon Connect instance ID
+                    ContactId: contactID
+                };
+                
+                try {
+                    const result = await connect.describeContact(params).promise();
+                    if (result.Contact && result.Contact.DisconnectTimestamp) {
+                        // Update InQueue attribute to false if contact has been disconnected
+                        await QueueData.update({ ContactID: contactID, InQueue: false });
+                    }
+                } catch (err) {
+                    console.error(`Error describing contact ${contactID}:`, err);
+                }
+            }
+            res.status(200).send("Disconnected calls updated successfully");
+        } catch (err) {
+            console.error("Error updating disconnected calls:", err);
+            res.status(500).send("Internal server error");
+        }
+    }
+
 }
 
 export default QueueDataController;
