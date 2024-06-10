f66e466a48aa669b7f8bea37ff9b6fff41b72fce
Gustavo Tellez <gusgoosegus1@gmail.com>
2024-06-10 16:52:46 -0600

    Instance ID from env
    Imported the instance id as env

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,3 +10,4 @@ export const AWS_ACCESS_KEY_ID:string = process.env.AWS_ACCESS_KEY_ID || '';
 export const AWS_SECRET_ACCESS_KEY:string = process.env.AWS_SECRET_ACCESS_KEY || '';
 export const AWS_ACCESS_KEY_ID_LENS:string = process.env.AWS_ACCESS_KEY_ID_LENS || '';
 export const AWS_SECRET_ACCESS_KEY_LENS:string = process.env.AWS_SECRET_ACCESS_KEY_LENS || '';
+export const AWS_INSTANCE_ID:string = process.env.AWS_INSTANCE_ID || '';
