a7693b69a58657f6e0382d569c04ce9678aba82e
AntonioFR10 <a01369043@itesm.mx>
2024-06-11 00:02:25 -0600

    Angeliu casi final

--- src/controllers/AgenteController.ts
+++ src/controllers/AgenteController.ts
@@ -1,3 +1,11 @@
+/**
+ * @author Angel Armando Marquez Curiel
+ * @author 
+ * @author
+ * 
+ * Controller in charge of managing the requests related to the agent
+ */ 
+
 import { Request,Response } from "express";
 import AbstractController from "./AbstractController";
 import connectLens from "../services/connectLensService";
@@ -6,33 +14,29 @@ import { connectService, customerProfilesService } from "../services/clientsServ
 import connect from "../services/connectService";
 import customer from "../services/customerService";
 
+
 class AgenteController extends AbstractController{
-    //Singleton
-    //Atributo de clase
+    
+    /* Singleton */
+    /* Class attributes and methods */
     private static _instance: AgenteController;
-    //Metodo de clase
     public static get instance():AbstractController{
         if(!this._instance){
             this._instance = new AgenteController("agente");
         }
         return this._instance;
     }
-    //Declarar todas las rutas del controlador
+    
+    /* Initializes the routes for the AgenteController. */
     protected initRoutes(): void {
-        
-        // Transcripción de prueba elegante (usada en los videos)
         this.router.get('/consultaTranscripcion2/:contactId',this.getTranscripcion2.bind(this));
         this.router.get('/prueba',this.getPrueba.bind(this));
         this.router.get('/consultaCustomerInfo/:contactId', this.getCustomerInfo.bind(this));
         this.router.get('/infoAgente/:agenteNombre', this.getInfoAgente.bind(this));
-        // Llamadas activas 
         this.router.get('/consultaContacts',this.getContacts.bind(this));
-        this.router.get('/verificarContacts',this.verificarConctacts.bind(this));
-
     }
 
-    
-
+    /*Function to get the number of the client of an active call*/
     private async getNumberActiva(initialContactId: string) {
         try {
             const input = { // GetContactAttributesRequest
@@ -55,54 +59,6 @@ class AgenteController extends AbstractController{
                 }
             ];
             return data;
-        }
-    }
-
-    private async getTranscripcion1(res: Response, contactId: string) {
-        try {
-            // const contactId = req.params.contactId;
-            const input = {
-                InstanceId: 'e730139b-8673-445e-8307-c3a9250199a2', // required
-                ContactId: contactId // required
-            };
-
-            // Obtener las métricas actuales
-            const command = await connectLens.listRealtimeContactAnalysisSegments(input).promise();
-            console.log([command, command.Segments[command.Segments.length - 1].Transcript?.Sentiment]);
-            res.json([command]);
-            
-        } catch (err) {
-            console.log(err);
-            const data = [
-                {
-                    "Segments": [
-                        {
-                            "Transcript": {
-                                "Id": "151fdea7-60ac-4136-8d76-3dc29b3c2ecd",
-                                "ParticipantId": "CUSTOMER",
-                                "ParticipantRole": "CUSTOMER",
-                                "Content": "...",
-                                "BeginOffsetMillis": 757,
-                                "EndOffsetMillis": 1275,
-                                "Sentiment": "NEUTRAL"
-                            }
-                        },
-                        {
-                            "Transcript": {
-                                "Id": "151fdea7-60ac-4136-8d76-3dc29b3c2ecd",
-                                "ParticipantId": "AGENT",
-                                "ParticipantRole": "AGENT",
-                                "Content": "...",
-                                "BeginOffsetMillis": 757,
-                                "EndOffsetMillis": 1275,
-                                "Sentiment": "NEUTRAL"
-                            }
-                        },
-                    ]
-                
-                }
-                ];
-            res.json(data);
         }
     }
 
@@ -133,6 +89,7 @@ class AgenteController extends AbstractController{
         }
     }
 
+    /*Function to get the information of the client with the help of @aws-sdk/client-customer-profiles*/
     private async getCustomerProfileActiva(phoneNumber: string) {
         try {
             const input = {
@@ -160,6 +117,7 @@ class AgenteController extends AbstractController{
         }
     }
 
+    /*Main function in charge to get all the information of the active calls*/
     private async getContacts(req: Request, res: Response) {
         try {
             const st = new Date(new Date().getTime() - (1000 * 60 * 60 * 24)); // Hace un día
@@ -186,23 +144,10 @@ class AgenteController extends AbstractController{
                     const numberData = await this.getNumberActiva(contact.Id? contact.Id : '');
                     
                     let customerNumber;
-                    let currentTime;
-                    let elapsedTime;
-                    let elapsedTimeInMilliseconds;
                     let sentimiento = 'NEUTRAL'
                     if (numberData && numberData[0] && numberData[0].Attributes) {
                         customerNumber = numberData[0].Attributes["Customer number"];
-                        currentTime = numberData[0].Attributes["CurrentTime"];
-
-                        // Calculate elapsed time
-                        const EnqueueTimestamp = new Date(contact.QueueInfo?.EnqueueTimestamp? contact.QueueInfo?.EnqueueTimestamp : '');
-                        const currentTimestamp = new Date(currentTime);
-                        elapsedTimeInMilliseconds = currentTimestamp.getTime() - EnqueueTimestamp.getTime();
-                        let elapsedTimeInSeconds = Math.floor(elapsedTimeInMilliseconds / 1000);
-                        let minutes = Math.floor(elapsedTimeInSeconds / 60);
-                        let seconds = elapsedTimeInSeconds % 60;
-
-                        elapsedTime = minutes + ":" + seconds;
+                        
                     } else {
                         throw new Error('No customer number found');
                     }
@@ -223,8 +168,6 @@ class AgenteController extends AbstractController{
                             NombreCliente: nombreCliente,
                             NombreAgente: nombreAgente,
                             EnqueueTimestamp: contact.QueueInfo?.EnqueueTimestamp,
-                            CurrentTime: currentTime,
-                            ElapsedTime: elapsedTime,
                             Sentimiento: sentimiento,
                             UserNameAgente: usernameAgente,
                             
@@ -253,32 +196,6 @@ class AgenteController extends AbstractController{
                 }
             ];
             res.status(400).json(llamada)
-        }
-    }
-
-    private async verificarConctacts(req: Request, res: Response) {
-        try {
-            const st = new Date(new Date().getTime() - (1000 * 60 * 60 * 24)); // Hace un día
-            const et = new Date(new Date().getTime() - (1000)); // Hace un segundo
-            const input = {
-                InstanceId: 'e730139b-8673-445e-8307-c3a9250199a2', // required
-                TimeRange: {
-                    Type: "INITIATION_TIMESTAMP", 
-                    StartTime: st, // Comienzo del día v2
-                    EndTime: et, // Final del día v2
-                }
-            };
-            const command = await connect.searchContacts(input).promise();
-            const result = ([command]);
-            const llamadaId = result[0].Contacts
-            .filter((contact) => !contact.DisconnectTimestamp)
-            .map((contact) => contact.Id);
-            res.status(200).json(llamadaId);
-            console.log(command);
-            
-        } catch (err) {
-            console.log(err);
-            res.status(500).send('Internal server error' + err);
         }
     }
 
